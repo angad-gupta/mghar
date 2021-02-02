115efbe85fef08ae9846aff427018a087d256187
Shyam Sundar Awal <shyam.sundar.awal@bidhee.com>
2021-02-02 17:40:43 +0545

    test

--- config/services.php
+++ config/services.php
@@ -46,11 +46,11 @@ return [
         'redirect' => env('FB_REDIRECT')
     ],
 
-    // 'google' => [
-    //     'client_id' => env('GOOGLE_CLIENT_ID'),         // Your Google Client ID
-    //     'client_secret' => env('GOOGLE_CLIENT_SECRET'), // Your Google Client Secret
-    //     'redirect' =>env('GOOGLE_CLIENT_REDIRECT'),
-    // ],
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),         // Your Google Client ID
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'), // Your Google Client Secret
+        'redirect' =>env('GOOGLE_CLIENT_REDIRECT'),
+    ],
 
 
 ];
