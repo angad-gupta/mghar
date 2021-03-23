2a00759915a6403932c786e6e1ad4630e736b4a4
Shyam Sundar Awal <shyam.sundar.awal@bidhee.com>
2021-03-23 15:24:35 +0545

    Test

--- app/Modules/Home/Http/Controllers/SubscriberController.php
+++ app/Modules/Home/Http/Controllers/SubscriberController.php
@@ -139,6 +139,32 @@ class SubscriberController extends Controller
 
         $data = $request->all();
 
+        $amount = $data['amount'];
+
+        $args = http_build_query(array(
+            'token' => 'test_secret_key_921dba0f9ac44e448bd5e478ef5de594',
+            'amount'  => $amount * 100;
+        ));
+
+        $url = "https://khalti.com/api/v2/payment/verify/";
+
+            # Make the call using API.
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS,$args);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+            $headers = ['Authorization: Key test_secret_key_f59e8b7d18b4499ca40f68195a846e9b'];
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+            // Response
+            $response = curl_exec($ch);
+            $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            curl_close($ch);
+
+            dd($response);
+
         $id = Auth::guard('subscriber')->user()->id;
         
         $amount = $data['amount'];
