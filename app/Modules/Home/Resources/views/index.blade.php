7a2059f593e4a462494e306035b7e2643c85dcf0
Shyam Sundar Awal <shyam.sundar.awal@bidhee.com>
2021-04-06 14:36:22 +0545

    Tst

--- app/Modules/Home/Resources/views/index.blade.php
+++ app/Modules/Home/Resources/views/index.blade.php
@@ -61,7 +61,7 @@
 
                                     @if($latest_videos->total() != 0)
                                     @foreach($latest_videos as $key => $value)
-                                    @php
+                                    @php 
                                     $coverimages = ($value->video_cover_image) ? asset($value->file_full_path).'/'.$value->video_cover_image : asset('admin/default.png');
                                     @endphp
                                     <div class="col-sm-12 col-md-4 col-lg-4 col-xl-2">
@@ -69,7 +69,8 @@
                                             <a href="{{ route('video-detail',['video_id'=>$value->id]) }}" class="featured-post-small-img">
                                                 <img src="{{$coverimages}}" alt="">
                                                 <a href="{{ route('add-to-wishlist',['video_id'=>$value->id]) }}" class="add-watchlist" data-toggle="tooltip" data-placement="top" title="Add to my Wishlist"><i class="fas fa-plus"></i></a>
-                                            </a>
+                                            </a> 
+                                            @dd($value->video_title);
                                             <div class="featured-post_content">
                                                 <a href="{{ route('video-detail',['video_id'=>$value->id]) }}">
                                                     <h5>{{$value->video_title}}</h5>
