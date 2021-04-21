6ce545085066a3dc7ba9dbf68ceaf9d87b730747
Angad Gupta <angadgupta@Angads-MacBook-Pro.local>
2021-04-21 14:56:57 +0545

    subscription pop up payment

--- app/Modules/Home/Resources/views/subscription.blade.php
+++ app/Modules/Home/Resources/views/subscription.blade.php
@@ -94,20 +94,20 @@
 
                                         @if(Auth::guard('subscriber')->check())
                                         <tr>
+                        
                                             <td scope="row"></td>
                                             <td class="text-center">
-                                                <button id="payment-button" type="Subscription" plan="one_month"
-                                                    amount="{{$subscription_payment->one_month_payment}}"
-                                                    class="payment-button btn btn-secondary"
-                                                    value="{{$subscription_payment->one_month_payment}}">Select</button>
-                                                <img src="{{ asset('home/images/ime.svg') }}" alt="ime" height="45"
-                                                    style="width: 45px;transform: scale(2.3);margin-left: 27px;"
-                                                    class="ime"
-                                                    data-payment="{{ $subscription_payment->one_month_payment }}"
-                                                    data-plan="one_month">
+                                                <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#paymentOneMonthModal">
+                                                    Select
+                                                </button>
+                                
                                             </td>
 
-                                            <td class="text-center"><button id="payment-button" type="Subscription"
+                                            <td class="text-center">
+                                                <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#paymentThreeMonthModal">
+                                                    Select
+                                                </button>
+                                                {{-- <button id="payment-button" type="Subscription"
                                                     plan="three_month"
                                                     value="{{$subscription_payment->three_month_payment}}"
                                                     amount="{{$subscription_payment->three_month_payment}}"
@@ -116,11 +116,15 @@
                                                     style="width: 45px;transform: scale(2.3);margin-left: 27px;"
                                                     class="ime"
                                                     data-payment="{{ $subscription_payment->three_month_payment }}"
-                                                    data-plan="three_month">
+                                                    data-plan="three_month"> --}}
 
                                             </td>
 
-                                            <td class="text-center"><button id="payment-button" type="Subscription"
+                                            <td class="text-center">
+                                                <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#paymentSixMonthModal">
+                                                    Select
+                                                </button>
+                                                {{-- <button id="payment-button" type="Subscription"
                                                     plan="six_month"
                                                     value="{{$subscription_payment->six_month_payment}}"
                                                     amount="{{$subscription_payment->six_month_payment}}"
@@ -129,10 +133,14 @@
                                                     style="width: 45px;transform: scale(2.3);margin-left: 27px;"
                                                     class="ime"
                                                     data-payment="{{ $subscription_payment->six_month_payment }}"
-                                                    data-plan="six_month">
+                                                    data-plan="six_month"> --}}
                                             </td>
 
-                                            <td class="text-center"><button id="payment-button" type="Subscription"
+                                            <td class="text-center">
+                                                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#paymentOneYearModal">
+                                                    Select
+                                                </button>
+                                                {{-- <button id="payment-button" type="Subscription"
                                                     plan="one_year" value="{{$subscription_payment->one_year_payment}}"
                                                     amount="{{$subscription_payment->one_year_payment}}"
                                                     class="payment-button btn btn-primary">Select</button>
@@ -140,7 +148,7 @@
                                                     style="width: 45px;transform: scale(2.3);margin-left: 27px;"
                                                     class="ime"
                                                     data-payment="{{ $subscription_payment->one_year_payment }}"
-                                                    data-plan="one_year">
+                                                    data-plan="one_year"> --}}
                                             </td>
                                         </tr>
                                         @else
@@ -163,6 +171,132 @@
         </div>
     </div>
 </div>
+
+
+<div class="modal fade" id="paymentOneMonthModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Select Payment Option</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body text-center">
+            <img src="https://thehimalayantimes.com/uploads/imported_images/wp-content/uploads/2019/08/Khalti-logo-1-1024x571.jpg" 
+            id="payment-button" type="Subscription" plan="one_month" height="42"
+            style="width: 100px;margin:20px;"
+            amount="{{$subscription_payment->one_month_payment}}"
+            class="payment-button "
+            value="{{$subscription_payment->one_month_payment}}"/>
+
+            <img src="{{ asset('home/images/ime.svg') }}" alt="ime" height="45" 
+            style="width: 45px;transform: scale(2.2);margin:20px;border-radius:10px;"
+            class="ime"
+            data-payment="{{ $subscription_payment->one_month_payment }}"
+            data-plan="one_month">
+
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="modal fade" id="paymentThreeMonthModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Select Payment Option</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body text-center">
+            <img src="https://thehimalayantimes.com/uploads/imported_images/wp-content/uploads/2019/08/Khalti-logo-1-1024x571.jpg" 
+            id="payment-button" type="Subscription" plan="one_month" height="42"
+            style="width: 100px;margin:20px;"
+            amount="{{$subscription_payment->three_month_payment}}"
+            class="payment-button "
+            value="{{$subscription_payment->three_month_payment}}"/>
+
+            <img src="{{ asset('home/images/ime.svg') }}" alt="ime" height="45" 
+            style="width: 45px;transform: scale(2.2);margin:20px;border-radius:10px;"
+            class="ime"
+            data-payment="{{ $subscription_payment->three_month_payment }}"
+            data-plan="one_month">
+
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="modal fade" id="paymentSixMonthModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Select Payment Option</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body text-center">
+            <img src="https://thehimalayantimes.com/uploads/imported_images/wp-content/uploads/2019/08/Khalti-logo-1-1024x571.jpg" 
+            id="payment-button" type="Subscription" plan="one_month" height="42"
+            style="width: 100px;margin:20px;"
+            amount="{{$subscription_payment->six_month_payment}}"
+            class="payment-button "
+            value="{{$subscription_payment->six_month_payment}}"/>
+
+            <img src="{{ asset('home/images/ime.svg') }}" alt="ime" height="45" 
+            style="width: 45px;transform: scale(2.2);margin:20px;border-radius:10px;"
+            class="ime"
+            data-payment="{{ $subscription_payment->six_month_payment }}"
+            data-plan="one_month">
+
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="modal fade" id="paymentOneYearModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Select Payment Option</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body text-center">
+            <img src="https://thehimalayantimes.com/uploads/imported_images/wp-content/uploads/2019/08/Khalti-logo-1-1024x571.jpg" 
+            id="payment-button" type="Subscription" plan="one_month" height="42"
+            style="width: 100px;margin:20px;"
+            amount="{{$subscription_payment->one_year_payment}}"
+            class="payment-button "
+            value="{{$subscription_payment->one_year_payment}}"/>
+
+            <img src="{{ asset('home/images/ime.svg') }}" alt="ime" height="45" 
+            style="width: 45px;transform: scale(2.2);margin:20px;border-radius:10px;"
+            class="ime"
+            data-payment="{{ $subscription_payment->one_year_payment }}"
+            data-plan="one_month">
+
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
 
 @stop
 
